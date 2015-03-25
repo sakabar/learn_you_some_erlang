d12668b77bd8dd99124135bb5766ec77fd49a093
sakaki <takafumi.sakakibara.pdx@gmail.com>
2015-03-25 16:23:22 +0900

    New: dolphins

---
+++ src/dolphins.erl
@@ -0,0 +1,36 @@
+-module(dolphins).
+-compile(export_all).
+
+dolphin1() ->
+  receive
+    do_a_flip ->
+      io:format("How about no?~n");
+    fish ->
+      io:format("So long and thanks for all the fish!~n");
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
+
+%% spawn/3はモジュール、関数とその引数を引数に取る
+dolphin2() ->
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?";
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n")
+  end.
+
+dolphin3() ->
+  receive
+    {From, do_a_flip} ->
+      From ! "How about no?",
+      dolphin3();
+    {From, fish} ->
+      From ! "So long and thanks for all the fish!";
+    _ ->
+      io:format("Heh, we're smarter than you humans.~n"),
+      dolphin3()
+  end.
+
